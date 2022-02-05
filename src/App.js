65f0126b4968ca04341fe816d607b67b57b401b1
Jenniferlala <kekvkikn@gmail.com>
2022-02-05 23:57:53 +0800

    Delete react default components.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import styled from 'styled-components';
+import Board from './components/Board';
 
+const StyledWrapper = styled.div`
+    min-height: 100vh;
+    background-color: var(--rice);
+`;
+const BoardContainer = styled.div`
+    text-align: center;
+    padding-top: 4rem;
+`;
+
+const EmptyBoard = [
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+];
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <StyledWrapper>
+                <BoardContainer>
+                    <Board />
+                </BoardContainer>
+            </StyledWrapper>
+        </div>
+    );
 }
 
 export default App;
