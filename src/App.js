ab1ff8f16990e50b9f379ac65ff53ddc8fb08ac4
Jenniferlala <kekvkikn@gmail.com>
2022-02-06 02:17:42 +0800

    Lift board state to App component.

--- src/App.js
+++ src/App.js
@@ -2,34 +2,126 @@ import { useState } from 'react';
 import './App.css';
 import styled from 'styled-components';
 import Board from './components/Board';
+import SolveButton from './components/button/Button';
 
 const StyledWrapper = styled.div`
     min-height: 100vh;
     background-color: var(--rice);
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding-top: 2rem;
 `;
 const BoardContainer = styled.div`
-    text-align: center;
-    padding-top: 4rem;
+    margin-bottom: 2rem;
 `;
 
-const EmptyBoard = [
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
-    ['.', '.', '.', '.', '.', '.', '.', '.', '.'],
+const defaultBoard = [
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
+    '.',
 ];
+
 function App() {
+    const [boardState, setBoardState] = useState(defaultBoard);
+
+    const handleClick = function (idx) {
+        const currState = boardState.slice(0);
+        let currValue = currState[idx];
+        if (currValue === '.') currValue = '1';
+        else if (currValue === '9') currValue = '.';
+        else currValue = (parseInt(currValue, 10) + 1).toString(10);
+
+        currState[idx] = currValue;
+        setBoardState(currState);
+    };
+
     return (
         <div className="App">
             <StyledWrapper>
                 <BoardContainer>
-                    <Board />
+                    <Board state={boardState} handleClick={handleClick} />
                 </BoardContainer>
+                <SolveButton />
             </StyledWrapper>
         </div>
     );
--- src/components/Board.js
+++ src/components/Board.js
@@ -3,96 +3,13 @@ import styled from 'styled-components';
 import Square from './Square';
 
 const BoardWrapper = styled.div`
-    width: min(36rem, 100vw);
-    aspect-ratio: 1/1;
+    width: min(36rem, 95vw);
+    aspect-ratio: 10/9;
     display: inline-grid;
     grid-template-columns: 1fr 1fr 1.1fr 1fr 1fr 1.1fr 1fr 1fr 1fr;
     grid-template-rows: 1fr 1fr 1.1fr 1fr 1fr 1.1fr 1fr 1fr 1fr;
     gap: 1%;
 `;
-const defaultBoard = [
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-    '.',
-];
 
 const SquareContainer = styled.div`
     padding-right: ${(props) => ((props.idx + 1) % 3 === 0 && (props.idx + 1) % 9 !== 0 ? '9.090909%' : 'none')};
@@ -100,25 +17,13 @@ const SquareContainer = styled.div`
         Math.floor(props.idx / 9) === 2 || Math.floor(props.idx / 9) === 5 ? '9.090909%' : 'none'};
 `;
 
-function Board() {
-    const [squareState, setSquareState] = useState(defaultBoard);
-
-    const handleClick = function (row, col) {
-        const currState = squareState.slice(0);
-        let currValue = currState[col];
-        if (currValue === '.') currValue = '1';
-        else if (currValue === '9') currValue = '.';
-        else currValue = (parseInt(currValue, 10) + 1).toString(10);
-
-        currState[col] = currValue;
-        setSquareState(currState);
-    };
+function Board({ state, handleClick }) {
     return (
         <BoardWrapper>
-            {squareState.map((val, idx) => {
+            {state.map((val, idx) => {
                 return (
                     <SquareContainer idx={idx}>
-                        <Square val={val} row={0} col={idx} handleClick={handleClick} />
+                        <Square val={val} idx={idx} handleClick={handleClick} />
                     </SquareContainer>
                 );
             })}
