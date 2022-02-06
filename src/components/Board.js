927418ccfb1a22035990585054c1b5f778cbeddc
Jenniferlala <kekvkikn@gmail.com>
2022-02-06 20:03:22 +0800

    Get states from App for rendering Board.

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Square from './Square';
 
@@ -9,6 +9,8 @@ const BoardWrapper = styled.div`
     grid-template-columns: 1fr 1fr 1.1fr 1fr 1fr 1.1fr 1fr 1fr 1fr;
     grid-template-rows: 1fr 1fr 1.1fr 1fr 1fr 1.1fr 1fr 1fr 1fr;
     gap: 1%;
+
+    pointer-events: ${(props) => (props.disable ? 'none' : 'auto')};
 `;
 
 const SquareContainer = styled.div`
@@ -17,15 +19,29 @@ const SquareContainer = styled.div`
         Math.floor(props.idx / 9) === 2 || Math.floor(props.idx / 9) === 5 ? '9.090909%' : 'none'};
 `;
 
-function Board({ state, handleClick }) {
+function Board({ state, emptyState, solved, handleClick }) {
     return (
-        <BoardWrapper>
+        <BoardWrapper disable={solved}>
             {state.map((val, idx) => {
-                return (
-                    <SquareContainer idx={idx}>
-                        <Square val={val} idx={idx} handleClick={handleClick} />
-                    </SquareContainer>
-                );
+                if (!solved) {
+                    return (
+                        <SquareContainer idx={idx}>
+                            <Square val={val} idx={idx} handleClick={handleClick} />
+                        </SquareContainer>
+                    );
+                } else {
+                    if (emptyState.includes(idx))
+                        return (
+                            <SquareContainer idx={idx}>
+                                <Square val={val} idx={idx} handleClick={handleClick} isAnswer={true} />
+                            </SquareContainer>
+                        );
+                    return (
+                        <SquareContainer idx={idx}>
+                            <Square val={val} idx={idx} handleClick={handleClick} />
+                        </SquareContainer>
+                    );
+                }
             })}
         </BoardWrapper>
     );
