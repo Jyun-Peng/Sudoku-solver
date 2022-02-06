5743f19cfd65889ca4eae8dd4f841d4f7e261ab0
Jenniferlala <kekvkikn@gmail.com>
2022-02-06 20:00:43 +0800

    Add states to SolveButton.

--- src/components/button/SolveButton.js
+++ src/components/button/SolveButton.js
@@ -3,25 +3,26 @@ import styled from 'styled-components';
 const ButtonWrapper = styled.div`
     width: min(20rem, 80vw);
     padding-bottom: 0.8rem;
-    background-color: var(--red);
+    background-color: var(${(props) => (props.disable ? '--gray-dark' : '--red')});
     border-radius: 1rem;
 
+    pointer-events: ${(props) => (props.disable ? 'none' : 'auto')};
     user-select: none;
     cursor: pointer;
 `;
 const ButtonSurface = styled.div`
     padding: 1rem 0;
-    background-color: var(--orange);
+    background-color: var(${(props) => (props.disable ? '--gray-light' : '--orange')});
     border-radius: inherit;
     font-size: 1.5rem;
     font-weight: 600;
-    color: var(--red-dark);
+    color: var(${(props) => (props.disable ? '--white' : '--red-dark')});
     text-align: center;
 `;
-function SolveButton({ handleClick }) {
+function SolveButton({ handleClick, disable }) {
     return (
-        <ButtonWrapper onClick={handleClick}>
-            <ButtonSurface>Solve</ButtonSurface>
+        <ButtonWrapper onClick={handleClick} disable={disable}>
+            <ButtonSurface disable={disable}>SOLVE!</ButtonSurface>
         </ButtonWrapper>
     );
 }
